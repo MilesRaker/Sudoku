4a8caf9c251394b5f7b9db919233891cd65d4ffe
MilesRaker <39108835+MilesRaker@users.noreply.github.com>
2022-05-03 14:30:08 -0700

    Added pickerButtons to blank gridsquares

--- Sudoku/PuzzleFrame.xaml.cs
+++ Sudoku/PuzzleFrame.xaml.cs
@@ -26,16 +26,97 @@ namespace Sudoku
         private PuzzleManager _puzzle;
         private string[,] _imageURIs = new string[9,9];
         private Image[,] _gridImages = new Image[9,9];
+        private Flyout[,] _flyoutGrid = new Flyout[9,9];
+        private Button[,] _buttonGrid = new Button[9,9];
+
         // frame constructor
         public PuzzleFrame()
         {
             _puzzle = new PuzzleManager(9, 25);
+            InitializeFlyoutPicker(2);
+            InitializeButtons();
             InitializeGridImages(2);
 
             this.InitializeComponent();
             this.InitializeGrid();
 
             //Image00.Source = new BitmapImage(new Uri("ms-appx:///Icons/Set2/Asset 9.svg")); 
+        }
+
+        private void InitializeButtons()
+        {
+            for (int x = 0; x < _puzzle.Generator.Size; x++)
+            {
+                for (int y = 0; y < _puzzle.Generator.Size; y++)
+                {
+                    if (_puzzle.Generator.PuzzleStart[x, y] == 0)
+                    {
+                        _buttonGrid[x, y] = new Button();
+                        _buttonGrid[x, y].HorizontalAlignment = HorizontalAlignment.Stretch;
+                        _buttonGrid[x, y].VerticalAlignment = VerticalAlignment.Stretch;
+                        _buttonGrid[x, y].Flyout = _flyoutGrid[x, y];
+                        Grid.SetRow(_buttonGrid[x, y], x);
+                        Grid.SetColumn(_buttonGrid[x, y], y);
+                    }
+                }
+            }
+        }
+
+        private void InitializeFlyoutPicker(int setNum)
+        {
+            for (int x = 0; x < _puzzle.Generator.Size; x++)
+            {
+                for (int y = 0; y < _puzzle.Generator.Size; y++)
+                {
+                    _flyoutGrid[x, y] = new Flyout();
+                    _flyoutGrid[x, y].Content = InitializePickerGrid(setNum);
+                }
+            }
+        }
+
+        private Grid InitializePickerGrid(int setNum)
+        {
+            Grid pickerGrid = new Grid();
+            ColumnDefinition col0 = new ColumnDefinition();
+            col0.Width = new GridLength(100);
+            ColumnDefinition col1 = new ColumnDefinition();
+            col1.Width = new GridLength(100);
+            ColumnDefinition col2 = new ColumnDefinition();
+            col2.Width = new GridLength(100);
+            RowDefinition row0 = new RowDefinition();
+            row0.Height = new GridLength(100);
+            RowDefinition row1 = new RowDefinition();
+            row1.Height = new GridLength(100);
+            RowDefinition row2 = new RowDefinition();
+            row2.Height = new GridLength(100);
+            pickerGrid.ColumnDefinitions.Add(col0);
+            pickerGrid.ColumnDefinitions.Add(col1);
+            pickerGrid.ColumnDefinitions.Add(col2);
+            pickerGrid.RowDefinitions.Add(row0);
+            pickerGrid.RowDefinitions.Add(row1);
+            pickerGrid.RowDefinitions.Add(row2);
+            Image[] pickerGridImages = AssignImagesToPickerGrid(setNum);
+            for(int i = 0; i < pickerGridImages.Length; i++)
+            {
+                pickerGrid.Children.Add(pickerGridImages[i]);
+            }
+            return pickerGrid;
+        }
+
+        private Image[] AssignImagesToPickerGrid(int setNum)
+        {
+            // create image array
+            // assign grid.row & grid.column
+            // assign image array as child to pickergrid
+            Image[] images = new Image[9];
+            for (int i = 0; i < images.Length; i++)
+            {
+                images[i] = new Image();
+                images[i].Source = new BitmapImage(new Uri($"ms-appx:///Icons/Set{setNum}/Asset {i + 1}.svg"));
+                Grid.SetRow(images[i], i / 3);
+                Grid.SetColumn(images[i], i % 3);
+            }
+            return images;
         }
 
         private void InitializeGrid()
@@ -79,12 +160,20 @@ namespace Sudoku
             board.RowDefinitions.Add(row7);
             board.RowDefinitions.Add(row8);
 
-            test.Children.Add(board);
+            NineFrame.Children.Add(board);
             for(int x = 0; x < _puzzle.Generator.Size; x++)
             {
                 for (int y = 0; y < _puzzle.Generator.Size; y++)
                 {
-                    board.Children.Add(_gridImages[x, y]);
+                    if(_puzzle.Generator.PuzzleStart[x, y] == 0)
+                    {
+                        board.Children.Add(_buttonGrid[x, y]);
+                        _buttonGrid[x, y].Content = _gridImages[x, y];
+                    }
+                    else
+                    {
+                        board.Children.Add(_gridImages[x, y]);
+                    }
                 }
             }
         }
